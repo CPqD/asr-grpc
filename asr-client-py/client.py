9e68cd6c3723afacf500b1734ad72d7b43a81a74
Fabiano Zaruch Chinasso <77642634+fzaruch@users.noreply.github.com>
2024-12-27 16:32:05 -0300

    Improve for saas (#2)
    * Melhorias para suporte a ambiente SaaS
    
    * Support cancel
    
    * Real time audi chunks
    
    * Atualiza documentação
    
    ---------
    
    Co-authored-by: Fabiano Zaruch Chinasso <fabiano.chinasso@ipinfusion.com>

--- asr-client-py/client.py
+++ asr-client-py/client.py
@@ -1,22 +1,100 @@
 import grpc
+import pydantic
+if pydantic.version.VERSION >= "2":
+    from pydantic_settings import BaseSettings
+else:
+    from pydantic import BaseSettings
 import RecognizeService_pb2_grpc as recognizeService
 import RecognizeService_pb2 as recognizeFields
+import os
 
+class Settings(BaseSettings):
+    server_url: str = ""
+    sl_token: str = ""
+    certificate_file: str = ""
+    metadata: str = ""
+    use_tls: bool = False
+    language_model_uri: str = ""
+    audio_name: str = ""
+    do_age_class: bool = False
+    do_gender_class: bool = False
+    do_emotion_class: bool = False
+    continuous_mode: bool = False
+    do_cancel: int = 0
+    timeout: int = 2
+    chunk_interval: float = 0.005
+    chunk_size: int = 800
+    encoding: str = "WAV"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+settings = Settings()
 
 def create_stub():
-    channel = grpc.insecure_channel("localhost:8026")
+    server=settings.server_url
+    if len(server) == 0:
+        print("Missing Server URL!!! \n")
+        exit(1)
+
+    use_tls=os.getenv("USE_TLS")
+    if settings.use_tls:
+        certificate=settings.certificate_file
+        if not certificate:
+            certificate="sl-cpqd-com-br.pem"
+            print("Missed certificate using: " + certificate)
+        f = open(certificate, 'rb').read()
+        i=f.decode().find("CN")
+        cn=f.decode()[i:]
+        cert_cn = cn[5:cn.find('\n')]
+        options = (('grpc.ssl_target_name_override', cert_cn,),)
+        options += (('grpc.tls_skip_hostname_verification', 'true',),)
+        channel_credentials = grpc.ssl_channel_credentials(f)
+        print("Secure connection to: " + server)
+        channel = grpc.secure_channel(server, channel_credentials, options=options)
+    else:
+        print("Insecure connection to: " + server)
+        channel = grpc.insecure_channel(server)
     stub = recognizeService.RecognizeServiceStub(channel)
     return stub
 
 
 def get_config():
     language_model = recognizeFields.RecognitionConfig.LanguageModel(content_type="text/uri-list",
-                                                                     uri="builtin:slm/general")
+                                                                     uri=settings.language_model_uri)
+    if settings.encoding == "WAV":
+        audio_encoding=recognizeFields.RecognitionConfig.AudioEncoding.WAV
+    else:
+        audio_encoding=recognizeFields.RecognitionConfig.AudioEncoding.LINEAR16
     return recognizeFields.RecognitionConfig(lm=[language_model],
-                                             audio_encoding=recognizeFields.RecognitionConfig.AudioEncoding.WAV)
+                                             audio_encoding=audio_encoding,
+                                             age_scores_enabled=settings.do_age_class,
+                                             gender_scores_enabled=settings.do_gender_class,
+                                             emotion_scores_enabled=settings.do_emotion_class,
+                                             continuous_mode=settings.continuous_mode)
 
 
 def print_message(message):
     print("\n#################################################")
     print("############# {} #############".format(message))
     print("#################################################\n")
+
+def print_result(response):
+    #print("Event: {}".format(response.event))
+    age="null"
+    emotion="null"
+    gender="null"
+    for r in response.result:
+            id=r.segment_index
+            if len(r.alternatives):
+                text=r.alternatives[0].text
+                if r.age_score.event == "AGE RESULT":
+                    age = r.age_score.age
+                if r.gender_score.event == "GENDER RESULT":
+                    gender = r.gender_score.gender
+                if r.emotion_class.event == "EMOTION RESULT":
+                    emotion = r.emotion_class.emotion
+                print("{}: age={} gender={} emotion={} - {}".format(id, age, gender, emotion, text))
+            else:
+                print("{}: status={}".format(id, r.status))
--- asr-client-py/streaming_recognize.py
+++ asr-client-py/streaming_recognize.py
@@ -1,36 +1,59 @@
 import client
+from client import settings
 import RecognizeService_pb2 as recognizeFields
 import os
-
+import time
+import grpc
 
 def execute_streaming_recognize():
     stub = client.create_stub()
     access_token=os.getenv('SL_TOKEN')
+    meta=settings.metadata
+    meta_par=meta_val='x-null'
+    if meta:
+        meta_par=meta[0:meta.find(':')]
+        meta_val=meta[meta.find(':')+1:]
     if access_token:
-        metadata = (('authorization', 'Bearer ' + access_token),)
-        responses = stub.StreamingRecognize(get_streaming_requests(), metadata=metadata)
+        metadata = (('authorization', 'Bearer ' + access_token), (meta_par, meta_val),)
+        responses = stub.StreamingRecognize(get_streaming_requests(), metadata=metadata, timeout=settings.timeout)
     else:
-        responses = stub.StreamingRecognize(get_streaming_requests())
+        responses = stub.StreamingRecognize(get_streaming_requests(), timeout=settings.timeout)
 
     client.print_message("Streaming Recognize")
-    for response in responses:
-        print(response)
+
+    try:
+        for response in responses:
+            #print(response)
+            client.print_result(response)
+    except grpc._channel._MultiThreadedRendezvous as e:
+        print(type(e).__name__)
+        print(e)
+        pass
 
 
 def get_streaming_requests():
-    audio_file = open("bank-transfira-8k.wav", "rb")
+    file_name = settings.audio_name
+    audio_file = open(file_name, "rb")
 
     config = create_streaming_config_request()
     yield config
 
-    audio_chunk = audio_file.read(1024)
-    next_chunk = audio_file.read(1024)
+    audio_chunk = audio_file.read(settings.chunk_size)
+    next_chunk = audio_file.read(settings.chunk_size)
+    i = 0;
     while next_chunk:
         yield recognizeFields.StreamingRecognizeRequest(media=audio_chunk, last_packet=False)
         audio_chunk = next_chunk
-        next_chunk = audio_file.read(1024)
+        next_chunk = audio_file.read(settings.chunk_size)
+        if settings.do_cancel and i > do_cancel:
+            yield recognizeFields.StreamingRecognizeRequest(stop=True)
+            print("Will Cancel")
+            break
+        i = i + 1
+        time.sleep(settings.chunk_interval)
 
-    yield recognizeFields.StreamingRecognizeRequest(media=audio_chunk, last_packet=True)
+    if settings.do_cancel == 0:
+        yield recognizeFields.StreamingRecognizeRequest(media=audio_chunk, last_packet=True)
 
     audio_file.close()
 
